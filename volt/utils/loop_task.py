4acfbb1f9767bc3f933b963586b346586f8f07d2
Lapis0875 <lapis0875@kakao.com>
2021-09-07 15:49:35 +0900

    feat: Update volt/utils/loop_task.py
    Fix problem about 'self' parameter :
    If @loop decorator is used in method expression, LoopTask will receive callback function as function object with 'self' parameter, not bound method object. I fixed it by defining '__get__(self, obj, objtype)' in LoopTask to hook attribute access on instance and inject 'self' attribute.

--- volt/utils/loop_task.py
+++ volt/utils/loop_task.py
@@ -1,9 +1,16 @@
 import asyncio
-import collections
 import typing
 from datetime import timedelta, time, timezone, datetime
+
+from .log import get_logger, DEBUG
 from ..types.type_hint import CoroutineFunction, AnyNumber, Decorator
 from .dtutil import utcnow, is_leap_year
+
+
+T = typing.TypeVar('T')
+
+
+logger = get_logger('volt.tasks', stream_level=DEBUG)
 
 
 class ZeroSecondsTaskNotSupported(Exception):
@@ -19,6 +26,7 @@ class LoopTask:
         '_kwargs',
         '_ignored_exceptions',
         '_task',
+        '_injected',
         '_days',
         '_hours',
         '_minutes',
@@ -28,7 +36,7 @@ class LoopTask:
         '_after_hook'
     )
 
-    def __init__(self, coro: CoroutineFunction, args, kwargs, days: int, hours: int, minutes: int, seconds: AnyNumber):
+    def __init__(self, coro: CoroutineFunction, args, kwargs, days: int, hours: int, minutes: int, seconds: int):
         try:
             self.loop = asyncio.get_running_loop()
         except RuntimeError:
@@ -37,8 +45,9 @@ class LoopTask:
         self._callback = coro
         self._args = args
         self._kwargs = kwargs or {}     # You can use keyword-arguments dictionary as namespace to pass on callback functions (before&after invoke, callback)
-        self._ignored_exceptions: typing.List[typing.Type[Exception]] = []
+        self._ignored_exceptions: list[typing.Type[Exception]] = []
         self._task = None
+        self._injected: object = None   # Used on LoopTasks in object to resolve 'self' parameter.
 
         # Looping delay info
         self._days = days
@@ -63,21 +72,27 @@ class LoopTask:
     def is_running(self) -> bool:
         return self._running
 
-    @property
-    def args(self) -> typing.Tuple:
-        return self._args
+    def __get__(self, obj: T, objtype: typing.Type[T]) -> 'LoopTask':
+        # Descriptor method used to define objects like `property`
+        # Defined to support object.some_task (type: LoopTask) work with 'self' parameter.
+        if obj is None:
+            return self
 
-    @args.setter
-    def args(self, new: typing.Tuple):
-        self._args = new
-
-    @property
-    def kwargs(self) -> typing.Mapping[str, typing.Any]:
-        return self._kwargs     # Maybe we need to freeze kwargs dict to control dictionary mutation.
-
-    @kwargs.setter
-    def kwargs(self, new: typing.Mapping[str, typing.Any]):
-        self._kwargs = new
+        # logger.debug(f'Injecting `self` attribute of object {obj} in {self.task_name}')
+        copy: LoopTask = LoopTask(
+            coro=self._callback,
+            args=self._args,
+            kwargs=self._kwargs,
+            days=self._days,
+            hours=self._hours,
+            minutes=self._minutes,
+            seconds=self._seconds,
+        )
+        copy._injected = obj
+        copy._before_hook = self._before_hook
+        copy._after_hook = self._after_hook
+        setattr(obj, self._callback.__name__, copy)
+        return copy
 
     def __call__(self, *args, **kwargs) -> typing.Coroutine:
         """
@@ -85,7 +100,11 @@ class LoopTask:
         :param args:
         :param kwargs:
         """
-        return self._callback(*self._args, **self._kwargs)
+        return self._callback(*args, **kwargs)
+
+    @property
+    def task_name(self) -> str:
+        return f'LoopTask({self._callback.__name__}, delay={self.total_delay})'
 
     def before_invoke(self, coro: CoroutineFunction):
         if not asyncio.iscoroutinefunction(coro):
@@ -104,46 +123,62 @@ class LoopTask:
         :param exc_value: Exception object.
         :param traceback: Exception traceback object.
         """
-        from sys import stderr
-        print(f'{exc_type.__name__} exception has been occurred while running youtube.LoopTask! Gracefully stop task.', file=stderr)
+        logger.error(f'{exc_type.__name__} exception has been occurred while running youtube.LoopTask! Gracefully stop task.', exc_info=exc_value)
         self.cancel()
 
     async def _run(self):
         args = self._args or tuple()
         kwargs = self._kwargs or {}
+        logger.debug(f'{self.task_name} is started.')
+
         if self._before_hook:
-            await self._before_hook(*args, **kwargs)
-        while self._running:
+            logger.debug(f'{self.task_name}.after_hook called.')
+            coro = self._before_hook(self._injected, *args, **kwargs) if self._injected is not None else self._before_hook(*args, **kwargs)
+            await coro
+
+        while True:
             try:
-                await self._callback(*args, **kwargs)
+                logger.debug(f'{self.task_name}.callback called.')
+                coro = self._callback(self._injected, *args, **kwargs) if self._injected is not None else self._callback(*args, **kwargs)
+                await coro
+                logger.debug(f'{self.task_name}.callback completed. Wait for next execution.')
             except self._ignored_exceptions as e:
+                logger.error(f'{self.task_name} >>> Exception raised, but it is handled.', exc_info=e)
                 await self._handle_exceptions(e.__class__, e, e.__traceback__)
             finally:
+                if not self._running:
+                    logger.debug(f'{self.task_name} is canceled. Break callback loop.')
+                    break
+                logger.debug(f'{self.task_name} : sleep for {self.total_delay.total_seconds()} seconds.')
                 await asyncio.sleep(delay=self.total_delay.total_seconds())
+
         if self._after_hook:
-            await self._after_hook(*args, **kwargs)
+            logger.debug(f'{self.task_name}.after_hook called.')
+            coro = self._after_hook(self._injected, *args, **kwargs) if self._injected is not None else self._after_hook(*args, **kwargs)
+            await coro
+        logger.debug(f'{self.task_name} is now closed.')
 
     def start(self, *args, **kwargs):
         if args:
-            if self._args is not None:
+            if self._args:
                 self._args += args
             else:
                 self._args = args
         if kwargs:
-            if self._kwargs is not None:
+            if self._kwargs:
                 self._kwargs.update(kwargs)
             else:
                 self._kwargs = kwargs
         self._running = True
-        self._task = self.loop.create_task(self._run(), name=f'LoopTask.run(id={id(self)})')
+        self._task = self.loop.create_task(self._run(), name=self.task_name)
         return self._task
 
     def cancel(self):
         self._running = False
 
 
-def loop(days: int = 0, hours: int = 0, minutes: int = 0, seconds: AnyNumber = 0) -> typing.Callable[[CoroutineFunction], LoopTask]:
-    def wrapper(coro: CoroutineFunction) -> LoopTask:
+def loop(days: int = 0, hours: int = 0, minutes: int = 0, seconds: int = 0):
+    def wrapper(coro: CoroutineFunction):
         if not asyncio.iscoroutinefunction(coro):
             raise TypeError('YoutubeEventLoop.LoopTask.callback must be coroutine function.')
         if not any((days, hours, minutes, seconds)):
